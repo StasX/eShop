adcf10379ff6c701fffbf9d431c7e59b060fb587
StasX <s.mestechkin@gmail.com>
2021-06-07 00:22:22 +0300

    Fix user model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
 const User = new Schema({
-    _id: {
+    id: {
         type: Number,
         required: true,
         unique: true
