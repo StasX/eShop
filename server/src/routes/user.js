f773042fa1f6f010821e18d191f0ca181acfad83
StasX <s.mestechkin@gmail.com>
2021-06-07 21:34:34 +0300

    Add register route

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const userCtrl = require("../controllers/userCtrl");
 
+// login
 router.post("/", userCtrl.login);
+// register
+router.put("/", userCtrl.register);
 
 module.exports = router;
