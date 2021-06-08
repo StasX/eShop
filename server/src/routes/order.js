187882085cfc5f2b555ff2201bda24dcf1738ab2
StasX <s.mestechkin@gmail.com>
2021-06-09 00:25:59 +0300

    Add order routes

---
+++ server/src/routes/order.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const orderCtrl = require("../controllers/orderCtrl");
+
+// get last order
+router.get("/order", orderCtrl.get);
+// add order
+router.post("/order", orderCtrl.add);
+
+module.exports = router;
