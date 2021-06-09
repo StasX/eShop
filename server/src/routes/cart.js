1b2a94ba10586e80b26235947dc329da56192db0
StasX <s.mestechkin@gmail.com>
2021-06-10 00:23:58 +0300

    Add cart routes

---
+++ server/src/routes/cart.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const cartCtrl = require("../controllers/orderCtrl");
+
+// get open cart
+router.get("/cart", cartCtrl.getOpen);
+// open cart
+router.post("/cart", cartCtrl.open);
+// update cart
+router.put("/cart/:id", cartCtrl.update);
+// close cart
+router.get("/cart/:id", cartCtrl.close);
+
+module.exports = router;
