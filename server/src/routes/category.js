2a8ab91d876debe299fea2b5d6820822afddf5a8
StasX <s.mestechkin@gmail.com>
2021-06-13 23:12:57 +0300

    Add category route

---
+++ server/src/routes/category.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const categoryCtrl = require("../controllers/categoryCtrl");
+
+// get categories
+router.get("/category", categoryCtrl.get);
+// add category
+router.post("/category", categoryCtrl.add);
+// update category
+router.post("/category/:id", categoryCtrl.update);
