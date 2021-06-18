e664d0ddf591a09196c995b487d1d3c1fc7ad0b3
StasX <s.mestechkin@gmail.com>
2021-06-19 01:28:25 +0300

    Add app.js

---
+++ client/src/app/app.js
@@ -0,0 +1,6 @@
+import * as angular from "../../bower_components/angular";
+import ngRoute from "../../bower_components/angular-route";
+
+const app = angular.module("app", [ngRoute]);
+
+module.exports = app;
