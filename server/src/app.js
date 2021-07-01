3a74ccf3ed43aa0d3f9ab5e9c61952d53ea3048d
StasX <s.mestechkin@gmail.com>
2021-07-02 00:23:41 +0300

    Fix cookie-parser middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const cart = require("./routes/cart");
 const category = require("./routes/category");
 const order = require("./routes/order");
@@ -8,7 +9,7 @@ const user = require("./routes/user");
 const app = express();
 
 app.use(express.static("public"));
-app.use(express.cookieParser());
+app.use(cookieParser());
 app.use(cart, category, order, product, user);
 
 module.exports = app;
