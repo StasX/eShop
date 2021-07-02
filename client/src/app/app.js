28ff8144ae2eb1e5d31d4b2e271fe2be957a6197
StasX <s.mestechkin@gmail.com>
2021-07-02 19:47:24 +0300

    Fix environment

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,4 +1,4 @@
-import * as angular from "../../bower_components/angular";
+import angular from "../../bower_components/angular";
 import ngRoute from "../../bower_components/angular-route";
 
 const app = angular.module("app", [ngRoute]);
