b3617d4e389b1bc55a82d8dd2f73ecfcc2b101da
StasX <s.mestechkin@gmail.com>
2021-07-10 14:55:09 +0300

    Add http requiests to store-content controller

--- client/src/app/controllers/storeContent.controller.js
+++ client/src/app/controllers/storeContent.controller.js
@@ -2,8 +2,15 @@ storeContentController.$inject = ["$scope", "$rootScope", "$http"];
 
 function storeContentController($scope, $rootScope, $http) {
     $scope.categories = [];
+    $scope.products = [];
     $http.get("/category").then((res) => {
-        $scope.categories = res.data;
+        $scope.categories = res.data.category;
+        $rootScope.categories = $scope.categories;
+    }, (err) => {
+        console.console.error(err);
+    });
+    $http.get("/product").then((res) => {
+        $scope.products = res.data.products;
     }, (err) => {
         console.console.error(err);
     });
