b86594d5eb5a07a1ff3515c21bc16703c74b37ef
StasX <s.mestechkin@gmail.com>
2021-07-12 23:25:22 +0300

    Rename file to storeContent.js

--- client/src/main.js
+++ client/src/main.js
@@ -10,7 +10,7 @@ import loginCtrl from "../src/app/controllers/login.controller";
 import managementCtrl from "../src/app/controllers/management.controller";
 import storeContentCtrl from "../src/app/controllers/storeContent.controller";
 
-import productShelfDirective from "../src/app/directives/productShelf";
+import productShelfDirective from "./app/directives/storeContent";
 import fileModelDirective from "../src/app/directives/fileModel";
 import productNameValidation from "../src/app/directives/productNameValidation";
 import priceValidation from "../src/app/directives/priceValidation";
